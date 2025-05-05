53c522e4e9782e436fb1f3854989295e1fafd4dc
Hamilton Insandara <hamisan36@gmail.com>
2025-05-05 17:32:09 -0500

    Start

---
+++ src/main/java/com/example/demo/infraestructure/entity/ConsultaMedica.java
@@ -0,0 +1,78 @@
+package com.example.demo.infraestructure.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "consultas_medicas")
+public class ConsultaMedica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String fecha;
+    private String diagnostico;
+    private String prescripcion;
+    private String observaciones;
+    private String medico;
+    private Long citaId;
+
+    public ConsultaMedica() {
+    }
+
+    public ConsultaMedica(String fecha, String diagnostico, String prescripcion, String observaciones, String medico,
+            Long citaId) {
+        this.fecha = fecha;
+        this.diagnostico = diagnostico;
+        this.prescripcion = prescripcion;
+        this.observaciones = observaciones;
+        this.medico = medico;
+        this.citaId = citaId;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+    public String getPrescripcion() {
+        return prescripcion;
+    }
+    public void setPrescripcion(String prescripcion) {
+        this.prescripcion = prescripcion;
+    }
+    public String getObservaciones() {
+        return observaciones;
+    }
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    public String getMedico() {
+        return medico;
+    }
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+    public Long getCitaId() {
+        return citaId;
+    }
+    public void setCitaId(Long citaId) {
+        this.citaId = citaId;
+    }
+
+    
+
+}
