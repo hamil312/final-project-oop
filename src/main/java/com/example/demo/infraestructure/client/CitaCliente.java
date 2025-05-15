fd613ba3f4fdb3cc8fe9273eb07668f299a4eee4
Hamilton Insandara <hamisan36@gmail.com>
2025-05-15 18:18:07 -0500

    Deployment

--- src/main/java/com/example/demo/infraestructure/client/CitaCliente.java
+++ src/main/java/com/example/demo/infraestructure/client/CitaCliente.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.domain.dto.CitaDTO;
 
-@FeignClient(name = "appointment-client", url = "https://citaspoo-production.up.railway.app/")
+@FeignClient(name = "appointment-client", url = "https://citaspoo-production.up.railway.app")
 public interface CitaCliente {
 
     @GetMapping("/citas/{id}")
