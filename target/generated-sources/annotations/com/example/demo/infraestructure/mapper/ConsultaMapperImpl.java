9345dc12ef4ba1151871cd9f9edc1b31f3f78a47
Hamilton Insandara <hamisan36@gmail.com>
2025-05-22 18:07:41 -0500

    ok

--- target/generated-sources/annotations/com/example/demo/infraestructure/mapper/ConsultaMapperImpl.java
+++ target/generated-sources/annotations/com/example/demo/infraestructure/mapper/ConsultaMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-19T21:46:09-0500",
-    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.0.v20250514-1000, environment: Java 21.0.7 (Eclipse Adoptium)"
+    date = "2025-05-22T18:06:45-0500",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 18.0.2.1 (Oracle Corporation)"
 )
 @Component
 public class ConsultaMapperImpl implements ConsultaMapper {
